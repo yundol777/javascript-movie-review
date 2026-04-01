2b23868434b9c37defa1c7af42b81de6d800bac8
yundoll <mar0722@naver.com>
2026-04-01 17:43:38 +0900

    feat: api 에러 처리 추가

--- src/controller/popularController.ts
+++ src/controller/popularController.ts
@@ -1,10 +1,15 @@
 import { getMovies } from "../getMovies";
 import { movieBanner } from "../movieBanner";
 import { movieListRender } from "../movieListRender";
+import { errorListRender } from "../view/errorListRender";
 
 export async function popularController(page: number) {
   const popularMovies: movieResponse | undefined = await getMovies(page);
-  if (popularMovies === undefined) return;
+  if (popularMovies === undefined) {
+    console.log("1");
+    errorListRender();
+    return;
+  }
   movieBanner(popularMovies.results[0]);
   movieListRender(popularMovies.results);
 }
--- src/controller/searchController.ts
+++ src/controller/searchController.ts
@@ -2,11 +2,18 @@ import { searchMovies } from "../searchMovies";
 import { movieListRender } from "../movieListRender";
 import { resetListRender } from "../resetListRender";
 import { emptyListRender } from "../view/emptyListRender";
+import { errorListRender } from "../view/errorListRender";
 
 export async function searchController(page: number, searchValue: string) {
-  const searchMoviesResult = await searchMovies(searchValue, page);
+  const searchMoviesResult: movieResponse | undefined = await searchMovies(
+    searchValue,
+    page,
+  );
 
-  if (!searchMoviesResult) return;
+  if (searchMoviesResult === undefined) {
+    errorListRender();
+    return;
+  }
   if (searchMoviesResult.results.length === 0) {
     emptyListRender();
     return;
