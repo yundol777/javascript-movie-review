be68938d5e9f809ccc8eebd7f67c011a00ef1461
yundoll <mar0722@naver.com>
2026-04-02 14:35:19 +0900

    test: cypress E2E 통합 테스트 추가

---
+++ cypress/e2e/home.cy.ts
@@ -0,0 +1,100 @@
+describe("홈 화면 테스트", () => {
+  beforeEach(() => {
+    cy.intercept("GET", "**/movie/popular?language=ko-KR&page=1").as(
+      "getMovies",
+    );
+    cy.visit("http://localhost:5173");
+  });
+
+  it("API 호출 확인", () => {
+    cy.wait("@getMovies").its("response.body.results").should("be.an", "array");
+  });
+
+  it("배너 안의 요소를 확인", () => {
+    cy.wait("@getMovies")
+      .its("response.body.results")
+      .then((results) => {
+        cy.get(".title").should("contain", results[0].title);
+        cy.get(".rate-value").should("contain", results[0].vote_average);
+        cy.get(".background-container")
+          .invoke("css", "background-image")
+          .should("include", results[0].poster_path);
+      });
+  });
+
+  it("리스트 안의 요소를 확인", () => {
+    cy.wait("@getMovies")
+      .its("response.body.results")
+      .then((results) => {
+        cy.get(".thumbnail").each(($el, index) => {
+          cy.wrap($el)
+            .should("have.attr", "src")
+            .and("include", results[index].poster_path);
+        });
+
+        cy.get(".item-rate").each(($el, index) => {
+          cy.wrap($el).should("contain", results[index].vote_average);
+        });
+
+        cy.get(".item-title").each(($el, index) => {
+          cy.wrap($el).should("contain", results[index].title);
+        });
+      });
+  });
+
+  it("더보기 버튼이 있는지 확인", () => {
+    cy.get(".thumbnail-add-button").should("exist");
+  });
+});
+
+describe("더보기 버튼 테스트", () => {
+  beforeEach(() => {
+    cy.intercept("GET", "**/movie/popular?language=ko-KR&page=1").as("page1");
+    cy.intercept("GET", "**/movie/popular?language=ko-KR&page=2").as("page2");
+
+    cy.visit("http://localhost:5173");
+  });
+
+  it("버튼 클릭 시 API 호출 확인", () => {
+    cy.get(".thumbnail-add-button").click();
+
+    cy.wait("@page2").its("response.body.results").should("be.an", "array");
+  });
+
+  it("리스트 안의 요소를 확인", () => {
+    let page1Results: Movies[];
+
+    cy.wait("@page1")
+      .its("response.body.results")
+      .then((results1) => {
+        page1Results = results1;
+      });
+
+    cy.get(".thumbnail-add-button").click();
+
+    cy.wait("@page2")
+      .its("response.body.results")
+      .then((results2) => {
+        const allResults = [...page1Results, ...results2];
+
+        cy.get(".thumbnail").should("have.length", allResults.length);
+
+        cy.get(".thumbnail").each(($el, index) => {
+          cy.wrap($el)
+            .should("have.attr", "src")
+            .and("include", allResults[index].poster_path);
+        });
+
+        cy.get(".item-title").each(($el, index) => {
+          cy.wrap($el).should("contain", allResults[index].title);
+        });
+
+        cy.get(".item-rate").each(($el, index) => {
+          cy.wrap($el).should(
+            "contain",
+            String(allResults[index].vote_average),
+          );
+        });
+      });
+  });
+});
