05f42340c281316add7d88866ae8d9a8a8d11892
yundoll <mar0722@naver.com>
2026-04-07 20:49:08 +0900

    Updates

---
+++ assets/index-CnKyFRNK.js
@@ -0,0 +1,472 @@
+(function polyfill() {
+  const relList = document.createElement("link").relList;
+  if (relList && relList.supports && relList.supports("modulepreload")) return;
+  for (const link of document.querySelectorAll('link[rel="modulepreload"]')) processPreload(link);
+  new MutationObserver((mutations) => {
+    for (const mutation of mutations) {
+      if (mutation.type !== "childList") continue;
+      for (const node of mutation.addedNodes) if (node.tagName === "LINK" && node.rel === "modulepreload") processPreload(node);
+    }
+  }).observe(document, {
+    childList: true,
+    subtree: true
+  });
+  function getFetchOpts(link) {
+    const fetchOpts = {};
+    if (link.integrity) fetchOpts.integrity = link.integrity;
+    if (link.referrerPolicy) fetchOpts.referrerPolicy = link.referrerPolicy;
+    if (link.crossOrigin === "use-credentials") fetchOpts.credentials = "include";
+    else if (link.crossOrigin === "anonymous") fetchOpts.credentials = "omit";
+    else fetchOpts.credentials = "same-origin";
+    return fetchOpts;
+  }
+  function processPreload(link) {
+    if (link.ep) return;
+    link.ep = true;
+    const fetchOpts = getFetchOpts(link);
+    fetch(link.href, fetchOpts);
+  }
+})();
+const token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMDRhNDVlZjM1NjVlYmU3MTNjOTllYjM3NGY4OWFlMCIsIm5iZiI6MTc3NDg1MDEzMC42NTI5OTk5LCJzdWIiOiI2OWNhMTA1MjY0ZTg2ODU0ODk4Mzk0ZjMiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.SPnfuj9NVH9rzYu7H5sSi7DubMehTR9nTdPOcrPnn04";
+const OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${token}`
+  }
+};
+class ResponseError extends Error {
+  type;
+  status;
+  constructor(message, type, status) {
+    super(message);
+    this.type = type;
+    this.status = status;
+  }
+}
+async function getMovies(page) {
+  try {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=${page}`,
+      OPTIONS
+    );
+    if (!response.ok) {
+      throw new ResponseError("[Error]: API 에러", "HTTP", response.status);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    if (error instanceof ResponseError) {
+      throw error;
+    }
+    throw new ResponseError("[Error]: 네트워크 에러", "NETWORK");
+  }
+}
+async function searchMovies(query, page) {
+  try {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/search/movie?language=ko-KR&query=${query}&page=${page}`,
+      OPTIONS
+    );
+    if (!response.ok) {
+      throw new ResponseError("[Error]: API 에러", "HTTP", response.status);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    if (error instanceof ResponseError) {
+      throw error;
+    }
+    throw new ResponseError("[Error]: 네트워크 에러", "NETWORK");
+  }
+}
+const SKELETON_NUMBER = 20;
+const ERROR_MESSAGE = {
+  DEFAULT: "문제가 발생했습니다. 잠시 후 다시 시도해주세요.",
+  NETWORK: "네트워크 연결을 확인한 뒤 다시 시도해주세요.",
+  HTTP: "영화 정보를 불러오지 못했습니다. 잠시 후 다시 시도해주세요.",
+  EMPTY: "검색 결과가 없습니다."
+};
+function isLastPage(moviesData) {
+  return moviesData.page === moviesData.total_pages;
+}
+async function morePopularController(page, movieListView, addButtonView) {
+  try {
+    movieListView.skeletonRender(SKELETON_NUMBER);
+    const popularMoviesData = await getMovies(page);
+    if (isLastPage(popularMoviesData)) addButtonView.hide();
+    movieListView.render(popularMoviesData.results);
+  } catch (error) {
+    if (error instanceof ResponseError) {
+      if (error.type === "HTTP") {
+        movieListView.errorRender(ERROR_MESSAGE.HTTP);
+        return;
+      }
+      if (error.type === "NETWORK") {
+        movieListView.errorRender(ERROR_MESSAGE.NETWORK);
+        return;
+      }
+      movieListView.errorRender(ERROR_MESSAGE.DEFAULT);
+    }
+  } finally {
+    movieListView.skeletonRemover();
+  }
+}
+async function moreSearchController(state, movieListView, addButtonView) {
+  try {
+    movieListView.skeletonRender(SKELETON_NUMBER);
+    const searchMoviesData = await searchMovies(
+      state.getSearchValue(),
+      state.getPage()
+    );
+    if (isLastPage(searchMoviesData)) addButtonView.hide();
+    movieListView.render(searchMoviesData.results);
+  } catch (error) {
+    if (error instanceof ResponseError) {
+      if (error.type === "HTTP") {
+        movieListView.errorRender(ERROR_MESSAGE.HTTP);
+        return;
+      }
+      if (error.type === "NETWORK") {
+        movieListView.errorRender(ERROR_MESSAGE.NETWORK);
+        return;
+      }
+      movieListView.errorRender(ERROR_MESSAGE.DEFAULT);
+    }
+  } finally {
+    movieListView.skeletonRemover();
+  }
+}
+async function popularController(page, movieListView, movieBannerView, addButtonView) {
+  try {
+    movieListView.reset();
+    movieListView.skeletonRender(SKELETON_NUMBER);
+    const popularMovies = await getMovies(page);
+    if (popularMovies.results.length === 0) {
+      movieListView.emptyRender();
+      addButtonView.hide();
+      return;
+    }
+    movieBannerView.render(popularMovies.results[0]);
+    movieListView.render(popularMovies.results);
+  } catch (error) {
+    if (error instanceof ResponseError) {
+      if (error.type === "HTTP") {
+        movieListView.errorRender(ERROR_MESSAGE.HTTP);
+        return;
+      }
+      if (error.type === "NETWORK") {
+        movieListView.errorRender(ERROR_MESSAGE.NETWORK);
+        return;
+      }
+      movieListView.errorRender(ERROR_MESSAGE.DEFAULT);
+    }
+  } finally {
+    movieListView.skeletonRemover();
+  }
+}
+async function searchController(state, movieListView, addButtonView) {
+  try {
+    movieListView.reset();
+    movieListView.skeletonRender(SKELETON_NUMBER);
+    const searchMoviesResult = await searchMovies(
+      state.getSearchValue(),
+      state.getPage()
+    );
+    if (searchMoviesResult.total_results === 0) {
+      movieListView.emptyRender();
+      addButtonView.hide();
+      return;
+    }
+    if (isLastPage(searchMoviesResult)) {
+      addButtonView.hide();
+    }
+    movieListView.render(searchMoviesResult.results);
+  } catch (error) {
+    if (error instanceof ResponseError) {
+      if (error.type === "HTTP") {
+        movieListView.errorRender(ERROR_MESSAGE.HTTP);
+        return;
+      }
+      if (error.type === "NETWORK") {
+        movieListView.errorRender(ERROR_MESSAGE.NETWORK);
+        return;
+      }
+      movieListView.errorRender(ERROR_MESSAGE.DEFAULT);
+    }
+  } finally {
+    movieListView.skeletonRemover();
+  }
+}
+class AddButtonView {
+  #section;
+  #handler;
+  constructor(handler) {
+    this.#section = document.querySelector("#add-button");
+    this.#handler = handler;
+    this.#binding();
+  }
+  #binding() {
+    this.#section?.addEventListener("click", () => {
+      this.#handler();
+    });
+  }
+  hide() {
+    if (this.#section) {
+      this.#section.style.display = "none";
+    }
+  }
+  show() {
+    if (this.#section) {
+      this.#section.style.display = "block";
+    }
+  }
+}
+const BANNER_IMAGE_URL = "https://image.tmdb.org/t/p/w1920_and_h800_multi_faces";
+class MovieBannerView {
+  #section;
+  #headerBar;
+  constructor() {
+    this.#section = document.querySelector(
+      ".background-container"
+    );
+    this.#headerBar = document.querySelector("#header-bar");
+  }
+  render(bannerMovie) {
+    if (!this.#section) return;
+    this.#section.style.backgroundImage = `url(${BANNER_IMAGE_URL + bannerMovie.poster_path})`;
+    this.#section.innerHTML = /*html*/
+    `
+      <div class="overlay" aria-hidden="true"></div>
+      <div class="top-rated-container">
+        <div class="top-rated-movie">
+          <div class="rate">
+            <span class="rate-value">${bannerMovie.vote_average}</span>
+          </div>
+          <div class="title">${bannerMovie.title}</div>
+          <button class="primary detail">자세히 보기</button>
+        </div>
+      </div>
+    `;
+  }
+  hide() {
+    if (!this.#section || !this.#headerBar) return;
+    this.#section.style.display = "none";
+    this.#headerBar.style.position = "relative";
+  }
+}
+class MovieListView {
+  #titleSection;
+  #listSection;
+  constructor() {
+    this.#listSection = document.querySelector(".thumbnail-list");
+    this.#titleSection = document.querySelector("#thumbnail-title");
+  }
+  setTitle(title) {
+    if (!this.#titleSection) return;
+    this.#titleSection.textContent = title;
+  }
+  render(movieList) {
+    const thumbnailImage = "https://media.themoviedb.org/t/p/w200";
+    movieList?.forEach((item) => {
+      const list = (
+        /*html*/
+        `
+      <li id=${item.id}>
+        <div class="item">
+          <img
+            class="thumbnail"
+            src=${thumbnailImage + item.poster_path}
+            alt=${item.title}
+            onerror="this.onerror=null; this.src='./src/asset/images/no-image.png'"
+        />
+          <div class="item-desc">
+            <p class="rate">
+              <img
+                src="./src/asset/images/star_empty.png"
+                class="star"
+              /><span class="item-rate">${item.vote_average}</span>
+            </p>
+            <strong class="item-title">${item.title}</strong>
+          </div>
+        </div>
+        </li>
+    `
+      );
+      if (!this.#listSection) return;
+      this.#listSection.insertAdjacentHTML("beforeend", list);
+    });
+  }
+  emptyRender() {
+    this.reset();
+    const emptyList = (
+      /*html*/
+      `
+        <li class="thumbnail-empty">
+            <img src="./src/asset/images/empty_icon.png" alt="empty list" class="empty-icon" />
+            <p class="empty-message">${ERROR_MESSAGE.EMPTY}</p>
+        </li>
+    `
+    );
+    if (!this.#listSection) return;
+    this.#listSection.innerHTML = emptyList;
+  }
+  errorRender(message) {
+    this.reset();
+    const emptyList = (
+      /*html*/
+      `
+        <li class="thumbnail-empty">
+            <img src="./src/asset/images/empty_icon.png" alt="empty list" class="empty-icon" />
+            <p class="empty-message">${message}</p>
+        </li>
+    `
+    );
+    if (!this.#listSection) return;
+    this.#listSection.innerHTML = emptyList;
+  }
+  skeletonRender(count = 20) {
+    const skeletonList = Array.from({ length: count }, () => {
+      return (
+        /*html*/
+        `
+      <li class="skeleton-card" aria-hidden="true">
+        <div class="item">
+          <div class="thumbnail skeleton-box"></div>
+          <div class="item-desc">
+            <p class="rate">
+              <span class="skeleton-star skeleton-box"></span>
+              <span class="skeleton-score skeleton-box"></span>
+            </p>
+            <strong class="skeleton-title skeleton-box"></strong>
+          </div>
+        </div>
+      </li>
+    `
+      );
+    }).join("");
+    if (!this.#listSection) return;
+    this.#listSection.insertAdjacentHTML("beforeend", skeletonList);
+  }
+  skeletonRemover() {
+    if (!this.#listSection) return;
+    const skeletonList = this.#listSection.querySelectorAll(".skeleton-card");
+    skeletonList.forEach((element) => element.remove());
+  }
+  reset() {
+    if (!this.#listSection) return;
+    this.#listSection.innerHTML = "";
+  }
+}
+class SearchView {
+  #handler;
+  #section;
+  constructor(handler) {
+    this.#handler = handler;
+    this.#section = document.querySelector("#search-form");
+    this.#binding();
+  }
+  #binding() {
+    this.#section?.addEventListener("submit", async (event) => {
+      event.preventDefault();
+      this.#handler();
+    });
+  }
+  getValue() {
+    const input = document.querySelector("#search-input");
+    if (!input) return "";
+    return input.value;
+  }
+}
+class AppState {
+  #page;
+  #isSearch;
+  #searchValue;
+  constructor() {
+    this.#page = 1;
+    this.#isSearch = false;
+    this.#searchValue = "";
+  }
+  reset() {
+    this.#page = 1;
+    this.#isSearch = false;
+    this.#searchValue = "";
+  }
+  resetSearch() {
+    this.#page = 1;
+    this.#isSearch = true;
+    this.#searchValue = "";
+  }
+  getPage() {
+    return this.#page;
+  }
+  getSearchValue() {
+    return this.#searchValue;
+  }
+  getIsSearch() {
+    return this.#isSearch;
+  }
+  increasePage() {
+    this.#page++;
+  }
+  setValue(nextValue) {
+    this.#searchValue = nextValue;
+  }
+  setIsSearch(nextIsSearch) {
+    this.#isSearch = nextIsSearch;
+  }
+}
+class MainController {
+  #movieListView;
+  #movieBannerView;
+  #searchView;
+  #addButtonView;
+  #appState;
+  constructor() {
+    this.#movieListView = new MovieListView();
+    this.#movieBannerView = new MovieBannerView();
+    this.#searchView = new SearchView(() => this.#handleSearch());
+    this.#addButtonView = new AddButtonView(() => this.#handleAddButton());
+    this.#appState = new AppState();
+  }
+  async init() {
+    const app = document.querySelector("#app");
+    if (!app) return;
+    this.#appState.reset();
+    await popularController(
+      this.#appState.getPage(),
+      this.#movieListView,
+      this.#movieBannerView,
+      this.#addButtonView
+    );
+  }
+  async #handleSearch() {
+    this.#appState.resetSearch();
+    this.#addButtonView.show();
+    this.#appState.setValue(this.#searchView.getValue());
+    searchController(this.#appState, this.#movieListView, this.#addButtonView);
+    this.#appState.setIsSearch(true);
+    this.#movieBannerView.hide();
+    this.#movieListView.setTitle(
+      `"${this.#appState.getSearchValue()}" 검색 결과`
+    );
+  }
+  async #handleAddButton() {
+    this.#appState.increasePage();
+    if (this.#appState.getIsSearch()) {
+      moreSearchController(
+        this.#appState,
+        this.#movieListView,
+        this.#addButtonView
+      );
+    } else {
+      morePopularController(
+        this.#appState.getPage(),
+        this.#movieListView,
+        this.#addButtonView
+      );
+    }
+  }
+}
+const mainController = new MainController();
+mainController.init();
